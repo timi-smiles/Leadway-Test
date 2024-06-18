f661151aa2272e1d6899f6b4de69f144f66eecad
timi-smiles <timioguns16@gmail.com>
2024-06-18 02:11:13 +0100

    lol commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,20 +2,20 @@
 import First from "./files/First";
 import Second from "./files/Second";
 import Third from "./files/Third";
-// import background from '../src/images/background1.jpeg'
+// import background from '../src/images/background.jpeg'
 
 
 function App() {
   return (
-    <div className="App" >
+    <div className="App">
+      <div className="full">
       <First></First>
       <Second></Second>
       <Third></Third>
+      </div>
     </div>
   );
 }
 
 export default App;
 
-
-// style={{ backgroundImage: `url(${background})`}}
--- my-app/src/files/First.jsx
+++ my-app/src/files/First.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from  '../images/images.jpeg'
+import Logo from '../images/logo1.jpeg'
 import { IoNotificationsOutline } from "react-icons/io5";
 // import background from '../images/background.jpeg';
 import './Me.css'
@@ -9,8 +10,7 @@ import './Me.css'
 const First = () => {
   return (
     <div className='center'>
-    
-        <img src={Image} alt="" />
+        <img src={Image} alt="" className='leadway'/>
         <IoNotificationsOutline size={30} className='icon'/>
     </div>
   )
